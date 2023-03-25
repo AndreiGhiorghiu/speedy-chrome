0900d18386d880e3a9745956ff34e3410ebcf1f0
Adrian Pislaru <adrian.pislaru@tremend.com>
2023-03-25 21:25:06 +0200

    Chat UPDATES

--- src/containers/ChatContainer/ChatContainer.jsx
+++ src/containers/ChatContainer/ChatContainer.jsx
@@ -1,10 +1,39 @@
 import * as React from 'react';
+import ChatTile from '../../components/ChatTile';
 
 export default function ChatContainer() {
+    const chatMessages = [
+        {
+            id: 1,
+            message: "Hey there! How can I help you?"
+        },
+        {
+            id: 2,
+            message: "Who is Adrian’s manager?"
+        },
+        {
+            id: 3,
+            message: "Adrian’s manager is Dragos."
+        },
+        {
+            id: 4,
+            message: "Tomorrow we have planning."
+        },
+        {
+            id: 5,
+            message: "Got it."
+        },
+    ];
 
     return (
-        <div>
-Chat container
+        <div style={{position:'relative', height: 'fit-content', flex: 1, paddingBottom: '70px'}}>
+            <div style={{height: 'calc(100vh - 182px)', overflowY: 'scroll'}}>
+                {chatMessages.map((elem, index) => (<ChatTile
+                    title={elem.message}
+                    left={index % 2 == 0 ? true : false}
+                />))}
+            </div>
+            
         </div>
     );
 }
--- src/containers/TasksBoard/TasksBoard.jsx
+++ src/containers/TasksBoard/TasksBoard.jsx
@@ -41,20 +41,21 @@ export default function SpeedyBoard() {
     };
 
     return (
-        <div style={{height: 'fit-content', flex: 1, paddingBottom: '70px'}}>
-            <>
+        <div style={{position:'relative', height: 'fit-content', flex: 1, paddingBottom: '70px'}}>
+            <div style={{height: 'calc(100vh - 182px)', overflowY: 'scroll'}}>
                 {tasksArr.map((elem) => (<TaskCard
                     title={elem.title}
                     label={elem.label}
                     storyPoints={elem.storyPoints}
                     onDelete={() => console.log('Delete clicked')}
                 />))}
-            </>
+            </div>
             <CustomButton
                 color="primary"
                 text="Upload to Jira"
                 onClick={addToJiraHandler}
                 backgroundColor="#EA5254"
+                sx={{position: 'fixed', bottom: '70px', left: 0, right: 0, margin: 'auto', width: 'fit-content'}}
             />
         </div>
     );
