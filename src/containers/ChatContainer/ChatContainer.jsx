c488febd433bbb960a4eba206b9a4bd4793f0930
Adrian Pislaru <adrian.pislaru@tremend.com>
2023-03-26 09:51:16 +0300

    asdada

--- src/containers/ChatContainer/ChatContainer.jsx
+++ src/containers/ChatContainer/ChatContainer.jsx
@@ -34,7 +34,7 @@ export default function ChatContainer() {
   const [chatMessages, setChatMessages] = useState([
     {
       id: 0,
-      message: 'Hey there! How can I help you?',
+      message: 'Hey there! How can I help you? asd asdsa adasda da as dasd as a asd asda sdas da da da',
       typing: false,
     },
   ]);
