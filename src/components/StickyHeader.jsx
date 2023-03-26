57d5279d282ee6c0037dbbef5b1b6323dcc1bf15
Adrian Pislaru <adrian.pislaru@tremend.com>
2023-03-26 05:48:26 +0300

    Add component name

--- src/components/StickyHeader.jsx
+++ src/components/StickyHeader.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography } from '@mui/material';
 
+import NewLogo from '../assets/img/newlogo.png';
+
 const StickyHeader = ({ title }) => {
     return (
-        <AppBar position="sticky" sx={{ height: 55, marginBottom: '10px', backgroundColor: 'black', borderBottom: '1px solid #3C3C3C' }}>
-            <Toolbar>
-                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                    {title}
-                </Typography>
-            </Toolbar>
+        <AppBar position="sticky" sx={{ height: 55, flexDirection: 'row', alignItems: 'center', marginBottom: '10px', backgroundColor: 'black', borderBottom: '1px solid #3C3C3C' }}>
+            <img style={{
+                width: '30px',
+                height: '30px',
+                marginLeft: '16px'
+            }} src={chrome.runtime.getURL(NewLogo)}/>
+            <div style={{width: '1px', marginLeft: '10px', backgroundColor: 'rgba(159, 165, 174, .6)', height: '25px'}} />
+            <span style={{fontSize: '14px', marginLeft: '10px', color: 'rgba(159, 165, 174, .6)'}}>Project name</span>
         </AppBar>
     );
 };
