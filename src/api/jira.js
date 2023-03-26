6dc9755c3a87f06d21bac0a5a7bf7516fe2510c1
Ghiorghiu Andrei <andrei.ghiorghiu@publicissapient.com>
2023-03-26 04:02:30 +0300

    floating button

--- src/api/jira.js
+++ src/api/jira.js
@@ -1,9 +1,12 @@
+import { method } from 'lodash';
+
 const jira = {};
 
 jira.checkAuth = async () => {
   const response = await fetch(
     'https://tremend.atlassian.net/rest/api/2/myself'
   );
+
   return response.json();
 };
 
@@ -12,9 +15,7 @@ jira.pushIssue = async (data) => {
     'https://aspirespeedy.atlassian.net/rest/api/2/issue',
     {
       method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
+      headers: {},
       redirect: 'follow',
 
       body: JSON.stringify({
@@ -35,4 +36,17 @@ jira.pushIssue = async (data) => {
   return response.json();
 };
 
+jira.getProjects = async () => {
+  const response = await fetch(
+    'https://aspirespeedy.atlassian.net/rest/internal/2/productsearch/search?counts=boards%3D5%2Cdashboards%3D5%2Cprojects%3D5%2Cfilters%3D5&type=boards%2Cdashboards%2Cprojects%2Cfilters'
+  );
+
+  const data = response.json();
+  const projects = data.find((item) => item.id === 'quick-search-projects');
+
+  return projects;
+};
+
 export default jira;
+
+// https://id.atlassian.com/login?login_hint=ct.96and@gmail.com&prompt=none&continue=https://aspirespeedy.atlassian.net/secure/BrowseProjects.jspa?selectedProjectType=software
--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,2 +1,6 @@
+import jira from '$apis/jira';
+
 console.log('This is the background page.');
 console.log('Put the background scripts here.');
+
+jira.getProjects().then((reply) => console.log('reply', reply));
